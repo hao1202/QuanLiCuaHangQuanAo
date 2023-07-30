134363362e40a49b9ff9a09cfd02e799c23e57f7
Tran Van Hao <tranvanhao2122003@gmail.com>
2023-07-30 22:02:04 +0700

    Create connection database in DBcontext

---
+++ quanLiCuaHangQuanAo.sql
@@ -0,0 +1,2 @@
+Create database QuanLiCuaHangQuanAo
+use QuanLiCuaHangQuanAo
--- src/main/java/repository/DBContext.java
+++ src/main/java/repository/DBContext.java
@@ -5,10 +5,57 @@
  */
 package repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author tranv
  */
 public class DBContext {
-    
+
+    public final static String USERNAME = "sa";
+    public final static String PASSWORD = "123";
+    public final static String URL = "jdbc:sqlserver://localhost:1433;"
+            + "databaseName=QuanLiCuaHangQuanAo;encrypt=false;";
+
+    static {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        Connection cn = null;
+        try {
+            cn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return cn;
+    }
+
+    public static void closeConnection(Connection cn) {
+        if (cn != null) {
+            try {
+                cn.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection connection = getConnection();
+        if (connection != null) {
+            System.out.println("thanh cong");
+        } else {
+            System.out.println("That bai");
+        }
+        closeConnection(connection);
+    }
 }
